03b6138d8e01cb070c6039163c2a0d0cec79de11
Kadriya Bazarova <Kadriya89@users.noreply.github.com>
2026-07-13 14:34:09 -0500

    no-mistakes(test): fix test fixtures: composer-lib symlink, tasks-axi skip, peer readiness

--- tests/fm-gotmp.test.sh
+++ tests/fm-gotmp.test.sh
@@ -58,6 +58,7 @@ make_fake_root() {
   ln -s "$ROOT/bin/fm-backend.sh" "$fake/bin/fm-backend.sh"
   ln -s "$ROOT/bin/backends/tmux.sh" "$fake/bin/backends/tmux.sh"
   ln -s "$ROOT/bin/fm-tmux-lib.sh" "$fake/bin/fm-tmux-lib.sh"
+  # fm-composer-lib.sh: fm-tmux-lib.sh sources it as a sibling.
   ln -s "$ROOT/bin/fm-composer-lib.sh" "$fake/bin/fm-composer-lib.sh"
   # fm-lock-lib.sh: teardown sources it for the shared lock-staleness proof.
   ln -s "$ROOT/bin/fm-lock-lib.sh" "$fake/bin/fm-lock-lib.sh"
