2c07b6673c6d0d387fb97ff977b118f2f17f9e9d
Firstmate <firstmate@example.invalid>
2026-09-01 03:48:15 +0000

    no-mistakes: apply CI fixes

--- tests/fm-supervision-instructions.test.sh
+++ tests/fm-supervision-instructions.test.sh
@@ -6,6 +6,7 @@ set -u
 . "$(dirname "${BASH_SOURCE[0]}")/lib.sh"
 
 TMP_ROOT=$(fm_test_tmproot fm-supervision-instructions)
+unset FM_HOME FM_STATE_OVERRIDE FM_CONFIG_OVERRIDE FM_ROOT_OVERRIDE FM_SUPERVISION_ACTOR FM_LEASE_HOLDER_PID
 RENDER="$ROOT/bin/fm-supervision-instructions.sh"
 
 test_selected_harness_block_only() {
