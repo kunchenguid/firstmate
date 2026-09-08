32eb5cda25906b45969b212f09bb66c947f2b97f
Jon Roosevelt <rooseveltadvisors@gmail.com>
2026-09-08 18:31:53 -0400

    fix(watch): accept relaunch metadata in PR polls

--- bin/fm-pr-lib.sh
+++ bin/fm-pr-lib.sh
@@ -315,7 +315,7 @@ fm_pr_metadata_identity_parse() {
           fm_pr_head_valid "$value" || post_pr_invalid=1
         fi
         ;;
-      x_request=*|x_request_ts=*|x_followups=*|x_platform=*|x_reply_max_chars=*)
+      x_request=*|x_request_ts=*|x_followups=*|x_platform=*|x_reply_max_chars=*|control_relaunch_tx=*)
         ;;
       *)
         [ "$seen_pr" -eq 0 ] || post_pr_invalid=1
